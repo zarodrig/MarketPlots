ab06de6cc82051c84538b30041dc883d6a036e26
Endwall <endwall@protonmail.com>
2020-12-21 22:08:06 -0700

    Plot volume in a subplot for yahoo finance data

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MarketPlots
-Plot Stock Market, Commodity, and Economic Data . 
+Plot Stock Market, Commodity, and Economic Data . <p>
+
+<h1> WINDOWS <\h1>
+
+Open a command window CMD or run in VS Code 
+
+> python marketPlots.py
+
+<h1>LINUX<\h1>
+
+Set the environment variable QT_QPA_PLATFORM=xcb , or egl or whatever
+$ export QT_QPA_PLATFORM=xcb
+
+place this in your .bashrc file 
+
+Now run marketPlots: Type the following command in a terminal.
+
+$ python marketPlots.py 
+
+Follow the directions for data download and plotting.
--- marketPlots.py
+++ marketPlots.py
@@ -3,14 +3,14 @@
 #  Title: marketPlots.py
 #  Author: Zorondras Rodriguez
 #  Creation Date: December 5, 2020
-#  Version: 0.09
-#  Revision Date: December 17, 2020
+#  Version: 0.11
+#  Revision Date: December 21, 2020
 #
 #  Description: Market Plots launcher
 ###########################################################
 
-import functions as fun
+import marketPlotsModule as mp
 
-fun.main_program()   # all of the program functions are in functions.py
+mp.main_program()   # all of the program functions are in functions.py
 
 ###################  END OF PROGRAM #####################################
